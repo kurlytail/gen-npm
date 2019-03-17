6a791c47ba51b7bb5b83850c7de91661c036386c
ashwin <ashwinkn@hotmail.com>
2019-03-17 10:59:44 -0500

    Upgraded to library versions 1.0

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+import Path from 'path';
+// eslint-disable-next-line no-undef
+const requireFunc = typeof __webpack_require__ === 'function' ? __non_webpack_require__ : require;
+const baseModule = requireFunc.main.children.find(module => module.id.includes('gen-npm/dist/npm.min.js'));
+let basePath;
+
+if (!baseModule) {
+    basePath = '.';
+} else {
+    basePath = Path.dirname(Path.dirname(baseModule.id));
+}
+
+const options = {
+    // eslint-disable-next-line no-undef
+    version: app.version,
+    map: [`${basePath}/templates/map.json`],
+    extension: [`${basePath}/templates/extensions`]
+};
+
+export default options;
